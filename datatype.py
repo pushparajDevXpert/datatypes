0d74ee0099ebf498ada0a61847a3809c651345aa
Pushparaj Supekar <supekar0573@gmail.com>
2026-03-14 12:01:01 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,101 @@
+'''
+Fundamental data types
+
+'''
+'''
+1.int data type==> Intger data types 
+
+1) int is on eof the fumdamental data types 
+2) value  without any decimal point is nothing but int
+3) it represent whole number as well as negative number 
+
+variable: it is name memory location where we can store data 
+ it is like a container 
+
+ syntax: var_name=value
+
+'''
+# price=100
+# runs=40
+# wicket=0
+# temp=-20
+
+# print("This price is :", price)
+# print(type(price)) #<class 'int'> it indicate that price is integer
+# print("The runs is:", runs)
+# print(type(runs))#<class 'int'>
+# print ("The wicket is:",wicket)
+# print(type(wicket))
+# print("The temperature is", temp)
+# print(type(temp))
+'''========================================='''
+'''
+2.float data type :>
+
+1)float is on of the decimal fundamental data types 
+2)value with decimal point is nothing but float
+'''
+# petrol_price=103.45
+# overs=20.4
+# balance=100.4
+
+# print(type(petrol_price))#<class 'float'>
+# print(type(overs))
+# print(type(balance))
+'''=========================='''
+'''
+3.bool data type:> 
+1)bool is one of the fundamental data types 
+2)for logical representation of data we use boolean
+
+there are 2 types of flages as
+True and False
+syantax= 
+     is_present=True
+     is_Secured=false
+internal value of True =1
+internal value of false =0
+
+int()      # they are the building data type
+float()
+bool()
+'''
+# is_present=True
+# is_secured=True
+# is_active=True
+# print(is_present)
+# print(type(is_present))#<class 'bool'>
+
+# res=is_present+is_active
+# print(res)
+# print(type(res))
+
+# data=float(True)
+# print(data)
+
+'''
+int 
+
+1)decimal : base value 10 system 0-9
+   v=7 
+   print(v) #7
+   print(type(v))#int
+2) binary base 2 system 0-1
+    
+
+
+'''
+
+
+# d=19
+# r1=bin(d)
+# print(type(r1))
+# print(type(d))
+
+
+# r2=oct(d)
+# r3=hex(d)
+# print(r2)
+# print(r3)
+# print(type(r2))
+# print(type(r3))
